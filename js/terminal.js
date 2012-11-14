22751f3a482520a6e317931da0b2700f8aa8595b
Chris Vuletich <cvuletich@gmail.com>
2012-11-13 22:16:52 -0600

    Updated portfolio

--- js/terminal.js
+++ js/terminal.js
@@ -32,6 +32,130 @@ var Dialog = {
         });
     }
 }
+var Portfolio = {
+  Index : 0,
+  Load : function() {
+    $('#portfolio-image').html('<img src="img/portfolio/' + Portfolio.Pages[Portfolio.Index].image + '" />');
+    $('#portfolio-project').html(Portfolio.Pages[Portfolio.Index].project);
+    $('#portfolio-technologies').html('<strong>Technologies: </strong> ' + Portfolio.Pages[Portfolio.Index].technologies);
+    $('#portfolio-role').html('<strong>Role: </strong> ' + Portfolio.Pages[Portfolio.Index].role);
+    $('#portfolio-synopsis').html('<strong>Synopsis:</strong>' + Portfolio.Pages[Portfolio.Index].synopsis);
+  },
+  Pages : [
+    {
+      'project' : 'DFCB Pulse',
+      'image' : 'pulse.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Python (Facebook, Twitter, Instagram, Flickr APIs), PHP, MySQL, JavaScript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'USPS Everywhere Locator',
+      'image' : 'uspseverywhere.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP, MySQL, JavaScript (jQuery, Google Maps API), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'VW Bluetooth Checker',
+      'image' : 'vwbluetooth.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP, Javascript, CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Kenworth PartsInsider',
+      'image' : 'kenworthpartsinsider.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Tomcat/JSP, Mssql, Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Peterbilt PartsInsider',
+      'image' : 'peterbiltpartsinsider.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Tomcat/JSP, Mssql, Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'TRP Parts',
+      'image' : 'trp.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP (Drupal), Javascript, CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'TRP Parts Mobile',
+      'image' : 'trpmobile.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP (Drupal), Javascript (jQuery, jQuery Mobile), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Cox Walking Dead Watch Party',
+      'image' : 'coxwalkingdead.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP, Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Cox Hattery\'s Press Site',
+      'image' : 'coxpress.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'DraftFCB Employee Directory App',
+      'image' : 'employeedirectory.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'PHP, Mssql, Javascript (jQuery, jQuery Mobile), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Electronic Arts Mass Effect 3 Press Site',
+      'image' : 'eamasseffect3.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'FancyChatter (side project)',
+      'image' : 'fancychatter.jpg',
+      'role' : 'Developer',
+      'technologies' : 'PHP, MySQL, Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : '10pix (side project)',
+      'image' : '10pix.jpg',
+      'role' : 'Developer',
+      'technologies' : 'PHP, MySQL, Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+    {
+      'project' : 'Soundbar (open source)',
+      'image' : 'soundbar.jpg',
+      'role' : 'Senior Developer @ DraftFCB',
+      'technologies' : 'Javascript (jQuery), CSS3, HTML',
+      'synopsis' : ''
+    },
+  ],
+  Next : function() {
+    Portfolio.Index++;
+    if (Portfolio.Index === Portfolio.Pages.length) {
+      Portfolio.Index = 0;
+    }
+    Portfolio.Load();
+  },
+  Previous : function() {
+    Portfolio.Index--;
+    if (Portfolio.Index < 0) {
+      Portfolio.Index = Portfolio.Pages.length - 1;
+    }
+    Portfolio.Load();
+  }
+}
 
 var d = new Date();
 var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
@@ -63,7 +187,7 @@ $(document).ready(function(){
             term.echo('');
         }else if (scommand[0] === 'blog') {
             $('#dialog .header .content').html('Blog');
-            $('#dialog div.content').html('<h2>First Post!</h2>So I\'ve finally decided to get my site up and running.  It\'s still a work in progress, but I hope to have it completely functional soon.'); 
+            $('#dialog div.content').html('<h2>First Post!</h2>So I\'ve finally decided to get my site up and running.  It\'s still a work in progress, but I hope to have it completely functional soon.');
             Dialog.Open(650, 600, 'center', 'center')
         } else if (scommand[0] === 'clear' || scommand[0] === 'cl') {
             $('#terminal').html();
@@ -79,12 +203,13 @@ $(document).ready(function(){
                 '   who          about me\n');
         }else if (scommand[0] === 'ping') {
             term.echo('\n' + 
-                '   Email: cvuletich@me.com\n' +
+                '   Email: cvuletich@gmail.com\n' +
                 '   Phone: (708) 721-7212\n' +
                 '   Twitter: @cvuletich\n\n');
         } else if (scommand[0] === 'portfolio') {
             $('#dialog .header .content').html('Portfolio');
-            $('#dialog div.content').html('Coming soon!<br />');
+            $('#dialog div.content').html('<div id="portfolio-project"></div><div id="portfolio-image"></div><div id="portfolio-previous" onclick="Portfolio.Previous()">&laquo; Prev</div><div id="portfolio-next" onclick="Portfolio.Next()">Next &raquo;</div><div id="portfolio-role"></div><div id="portfolio-technologies"></div><div id="portfolio-synopsis"></div>');
+            Portfolio.Load();
             Dialog.Open(650, 600, 'center', 'center')
         }else if (scommand[0] === 'resume') {
             term.echo('');
@@ -99,7 +224,7 @@ $(document).ready(function(){
             term.echo(scommand[0] + ': Permission denied');
         }else if (scommand[0] === 'who') {
             term.echo('\n' + 
-                '   Email: cvuletich@me.com\n' +
+                '   Email: cvuletich@gmail.com\n' +
                 '   Phone: (708) 721-7212\n' +
                 '   Twitter: @cvuletich\n\n');
         } else {
